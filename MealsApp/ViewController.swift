8511f0505e65d7f6a06456301a27758008c9083b
akmerejf <akmerejf@gmail.com>
2020-07-07 01:18:16 -0300

    Add XCode ios project

---
+++ MealsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealsApp
+//
+//  Created by Akme-re Monteiro de Almeida on 07/07/20.
+//  Copyright © 2020 Akme-re Monteiro de Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
